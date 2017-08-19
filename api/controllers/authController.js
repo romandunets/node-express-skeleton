d488240a11a3a5965186c5a086217e73379d7f55
Roman Dunets <roman.dunets.email@gmail.com>
2017-08-19 17:36:10 +0300

    Add verify token function to auth controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -45,3 +45,31 @@ exports.authenticate = function(req, res) {
     }
   });
 }
+
+exports.verify_token = function(req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, 'password', function(err, decoded) { // TODO: to settings  
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token, return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+  }
+};
