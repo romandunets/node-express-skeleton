732f73c37c5a8deee767398106080817f29b4bd1
Roman Dunets <roman.dunets.email@gmail.com>
2017-09-25 21:43:46 +0300

    Update scripts to support dev env by default

--- db/export.sh
+++ db/export.sh
@@ -1,9 +1,19 @@
-collections=('users' 'items')
+#!/bin/bash
+
+collections=("users" "items")
+environment="dev"
+
+if [[ "$1" != "" ]]
+then
+  environment=$1
+fi
+
+echo "Using env = $environment"
 
 for collection in "${collections[@]}"
 do
   echo "Exporting $collection"
-  mongoexport --db "node-express-skeleton-$1" --collection $collection --out ./db/seeds/$collection.json --jsonArray --pretty
+  mongoexport --db "node-express-skeleton-$environment" --collection $collection --out ./db/seeds/$collection.json --jsonArray --pretty
 done
 
 echo 'Done'
--- db/import.sh
+++ db/import.sh
@@ -1,9 +1,19 @@
-collections=('users' 'items')
+#!/bin/bash
+
+collections=("users" "items")
+environment="dev"
+
+if [[ "$1" != "" ]]
+then
+  environment=$1
+fi
+
+echo "Using env = $environment"
 
 for collection in "${collections[@]}"
 do
   echo "Importing $collection"
-  mongoimport --db "node-express-skeleton-$1" --collection $collection --file ./db/seeds/$collection.json --jsonArray
+  mongoimport --db "node-express-skeleton-$environment" --collection $collection --file ./db/seeds/$collection.json --jsonArray
 done
 
-echo 'Done'
+echo "Done"
